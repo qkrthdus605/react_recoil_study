6ef309581563dbbb28d14dd4101e7f32d40c11fe
qkrthdus605 <qkrthdus605@gmail.com>
2022-02-19 21:52:33 +0900

    commit

---
+++ src/components/GridExample.js
@@ -0,0 +1,13 @@
+
+function GridExample() {
+  return (
+    <div>
+      <p>Select Color : </p>
+      <button className='button'>Red</button>
+      <button className='button'>Blue</button>
+      <button className='button'>White</button>
+    </div>
+  );
+};
+
+export default GridExample;
