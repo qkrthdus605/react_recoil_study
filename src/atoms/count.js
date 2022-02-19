1f2f7a2376cfb71701b9e11994e1a0cd80bdbf14
qkrthdus605 <qkrthdus605@gmail.com>
2022-02-19 21:19:38 +0900

    first commit

---
+++ src/atoms/count.js
@@ -0,0 +1,21 @@
+import { atom } from 'recoil';
+
+export const countState = atom({
+  key: 'contState',
+  default: 30,
+});
+
+export const RedcountState = atom({
+  key: 'RedcountState',
+  default: 30,
+});
+
+export const GreencountState = atom({
+  key: 'GreencountState',
+  default: 30,
+});
+
+export const BluecountState = atom({
+  key: 'BluecountState',
+  default: 30,
+});
---
+++ src/components/Counter.js
@@ -0,0 +1,33 @@
+import { useRecoilState } from 'recoil';
+import { RedcountState, GreencountState, BluecountState } from '../atoms/count';
+
+function Counter() {
+  const [RedCount, setRedCount] = useRecoilState(RedcountState);
+  const [GreenCount, setGreenCount] = useRecoilState(GreencountState);
+  const [BlueCount, setBlueCount] = useRecoilState(BluecountState);
+  
+  return (
+    <div>
+      <p>r: {RedCount}</p>
+      <button onClick={() => setRedCount(RedCount + 5)}>+</button>
+      <button onClick={() => setRedCount(RedCount - 5)}>-</button>
+      <br />
+      <p>g: {GreenCount}</p>
+      <button onClick={() => setGreenCount(GreenCount + 5)}>+</button>
+      <button onClick={() => setGreenCount(GreenCount - 5)}>-</button>
+      <br />
+      <p>b: {BlueCount}</p>
+      <button onClick={() => setBlueCount(BlueCount + 5)}>+</button>
+      <button onClick={() => setBlueCount(BlueCount - 5)}>-</button>
+      <br />
+      <div style={{
+        width: '100px',
+        height: '100px',
+        border: '1px solid black',
+        backgroundColor: `rgb(${RedCount}, ${GreenCount}, ${BlueCount})`,
+      }}></div>
+    </div>
+  );
+};
+
+export default Counter;
